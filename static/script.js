ec15bcb3f644572bb1a68be34d621662aa14ef23
21ryans <43468306+21ryans@users.noreply.github.com>
2020-07-03 10:23:28 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,14 +1,33 @@
-function saveToFirebase(email) {
-    var emailObject = {
-        email: email
+var firebaseConfig = {
+apiKey: "AIzaSyDoznQPVPPLaUzKXn9sVEJAK8easxP-6rg",
+authDomain: "bob-erqrfo.firebaseapp.com",
+databaseURL: "https://bob-erqrfo.firebaseio.com",
+projectId: "bob-erqrfo",
+storageBucket: "bob-erqrfo.appspot.com",
+messagingSenderId: "465444855156",
+appId: "1:465444855156:web:162dd83c81189bea856021"
+};
+firebase.initializeApp(firebaseConfig);
+
+function fpush(type, title, url, user_content, user_update, admin_content, admin_update, location, parent_post_id, poster) {
+    var obj = {
+        type: type,
+        title: title,
+        url: url,
+        user_content: user_content,
+        user_update: user_update,
+        admin_content: admin_content,
+        admin_update: admin_update,
+        location: location,
+        parent_post_id: parent_post_id,
+        poster: poster
     };
-
-    firebase.database().ref('subscription-entries').push().set(emailObject)
-        .then(function(snapshot) {
-            alert('ok')
-        }, function(error) {
-            alert('error' + error)
-        });
+    firebase.database().ref('posts').push().set(obj)
+        .then(
+            function(snapshot) {
+            }, 
+            function(error) {
+                alert(error);
+            }
+        );
 }
-
-saveToFirebase(email);
